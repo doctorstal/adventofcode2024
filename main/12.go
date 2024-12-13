3ed271276ae40588f2716ad51e042eb683d2d77c
Oleksii Stadnyk <doctorstal@gmail.com>
2024-12-13 08:36:45 +0100

    Dat 12 (p2)

--- main/12.go
+++ main/12.go
@@ -7,9 +7,9 @@ import (
 	"os"
 )
 
-const size12 = 140
-
 func twelfthDay() {
+	const size12 = 140
+
 	fmt.Println("Twelfth day go")
 	// Read input
 
@@ -45,61 +45,93 @@ func twelfthDay() {
 	}
 
 	var totalCost int64 = 0
+	var costWithDiscount int64 = 0
 
 	for i, row := range plots {
 		for j, plantType := range row {
 			if mapped[i][j] {
 				continue
 			}
-			area, perimeter := calcAreaAndPerimeter(plots, mapped, i, j, plantType)
+			seenSides := make(map[string]bool)
+			area, perimeter, sides := calcAreaAndPerimeter(plots, mapped, seenSides, i, j, plantType)
 			totalCost += int64(area) * int64(perimeter)
+			costWithDiscount += int64(area) * int64(sides)
 		}
 	}
 
 	fmt.Printf("TotalCost: %v\n", totalCost)
+	fmt.Printf("costWithDiscount: %v\n", costWithDiscount)
 
+}
+
+func isStepOutside(plots [][]byte, i, j int, plantType byte) bool {
+	return i < 0 || j < 0 || i >= len(plots) || j >= len(plots) || plots[i][j] != plantType
 }
 
 /*
 *
 
-	returns area, perimeter
+	returns area, perimeter, sides
 */
-func calcAreaAndPerimeter(plots [][]byte, mapped [][]bool, i, j int, plantType byte) (int, int) {
-	if i < 0 || j < 0 || i >= len(plots) || j >= len(plots) || plots[i][j] != plantType {
-		// Do not add to area, add to perimeter, stop looking
-		return 0, 1
-	}
+func calcAreaAndPerimeter(plots [][]byte, mapped [][]bool, seenSides map[string]bool, i, j int, plantType byte) (int, int, int) {
+
 	if mapped[i][j] {
 		// Ignore already mapped, stop looking
-		return 0, 0
+		return 0, 0, 0
 	}
 	mapped[i][j] = true
 
 	// Plot grows same type of plant and is not mapped before
-	area, perimeter := 1, 0
+	area, perimeter, sides := 1, 0, 0
 
-	var a, p int
-	// look right
-	a, p = calcAreaAndPerimeter(plots, mapped, i+1, j, plantType)
-	area += a
-	perimeter += p
+	key := func(a, b, c, d int) string {
+		return fmt.Sprintf("%d:%d|%d:%d", a, b, c, d)
+	}
 
-	// look left
-	a, p = calcAreaAndPerimeter(plots, mapped, i-1, j, plantType)
-	area += a
-	perimeter += p
+	visit := func(di, dj, mi, mj int) {
+		var a, p, s int
+		if isStepOutside(plots, i+di, j+dj, plantType) {
+			// Do not add to area, add to perimeter
+			a, p = 0, 1
+			if !seenSides[key(i, j, i+di, j+dj)] {
+				// mark seen side forward
 
-	// look up
-	a, p = calcAreaAndPerimeter(plots, mapped, i, j-1, plantType)
-	area += a
-	perimeter += p
+				// (i, j+1)(di=0, dj=1, mi=1, mj=0) i+m*mi, j+m*mj : i + m * mi + di, j + m * mj + di
+				// (i, j-1)(di=0, dj=1, mi=1, mj=0) i+m, j : i+m, j-1
+				// (i+1, j)(di=1, dj=0, 0, 1) i, j+m : i+1, j+m
+				// (i-1, j)(di=-1, dj=0, 0, 1) i, j+m : i-1, j+m
+				for m := 0; m < len(plots); m++ {
+					if !isStepOutside(plots, i+m*mi, j+m*mj, plantType) && isStepOutside(plots, i+m*mi+di, j+m*mj+dj, plantType) {
+						seenSides[key(i+m*mi, j+m*mj, i+m*mi+di, j+m*mj+dj)] = true
+					} else {
+						break
+					}
+				}
+				// mark seen side backward
 
-	// look down
-	a, p = calcAreaAndPerimeter(plots, mapped, i, j+1, plantType)
-	area += a
-	perimeter += p
+				for m := 0; m > -len(plots); m-- {
 
-	return area, perimeter
+					if !isStepOutside(plots, i+m*mi, j+m*mj, plantType) && isStepOutside(plots, i+m*mi+1*di, j+m*mj+1*dj, plantType) {
+						seenSides[key(i+m*mi, j+m*mj, i+m*mi+1*di, j+m*mj+1*dj)] = true
+					} else {
+						break
+					}
+				}
+				s = 1
+			}
+		} else {
+			a, p, s = calcAreaAndPerimeter(plots, mapped, seenSides, i+di, j+dj, plantType)
+		}
+		area += a
+		perimeter += p
+		sides += s
+	}
+
+	visit(0, 1, 1, 0)
+	visit(0, -1, 1, 0)
+	visit(1, 0, 0, 1)
+	visit(-1, 0, 0, 1)
+
+	return area, perimeter, sides
 
 }
