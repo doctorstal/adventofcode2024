19511124820134c4ed7df5de5b06a63e81e587dd
Oleksii Stadnyk <doctorstal@gmail.com>
2024-12-22 16:01:50 +0000

    Day 6 part 2

--- main/6.go
+++ main/6.go
@@ -14,31 +14,66 @@ type Guard struct {
 	dy int
 }
 
+func (guard *Guard) getGuardByte() byte {
+	if guard.dy == -1 {
+		return '^'
+	}
+	if guard.dy == 1 {
+		return 'v'
+	}
+	if guard.dx == -1 {
+		return '<'
+	}
+	if guard.dx == 1 {
+		return '>'
+	}
+	return 'X'
+}
+
+func (guard *Guard) getPathKey() string {
+	return fmt.Sprintf("%d,%d,%s", guard.x, guard.y, string(guard.getGuardByte()))
+}
+
+func (guard *Guard) moveInArea(area [][]byte, aw, ah int) bool {
+	guard.x += guard.dx
+	guard.y += guard.dy
+	if guard.x < 0 || guard.x >= aw || guard.y < 0 || guard.y >= ah {
+		return false
+	}
+
+	if area[guard.y][guard.x] == '#' {
+		// step back
+		guard.x -= guard.dx
+		guard.y -= guard.dy
+		// rotate 90
+		guard.dy, guard.dx = guard.dx, -guard.dy
+	}
+	return true
+}
+
 func sixthDay() {
 	fmt.Println("Day 6: Guard Gallivant")
 
 	area := readMappedArea("input6.txt")
+	// area := readMappedArea("input6example.txt")
 	guard := findGuard(area)
 
 	fmt.Printf("guard: %v\n", *guard)
 
 	ah := len(area)
 	aw := len(area[0])
+
+	obstacleOptions := 0
+	path := make(map[string]bool)
 	for {
-		area[guard.y][guard.x] = 'X'
-
-		guard.x += guard.dx
-		guard.y += guard.dy
-		if guard.x < 0 || guard.x >= aw || guard.y < 0 || guard.y >= ah {
-			break
+		if canLoop(area, guard, path) {
+			obstacleOptions++
 		}
+		path[guard.getPathKey()] = true
+		area[guard.y][guard.x] = guard.getGuardByte()
 
-		if area[guard.y][guard.x] == '#' {
-			// step back
-			guard.x -= guard.dx
-			guard.y -= guard.dy
-			// rotate 90
-			guard.dy, guard.dx = guard.dx, -guard.dy
+		if !guard.moveInArea(area, aw, ah) {
+			break
 		}
 	}
 
@@ -46,7 +81,7 @@ func sixthDay() {
 
 	for _, row := range area {
 		for _, tile := range row {
-			if tile == 'X' {
+			if tile != '.' && tile != '#' {
 				positions++
 			}
 		}
@@ -54,32 +89,57 @@ func sixthDay() {
 	}
 
 	fmt.Printf("positions: %v\n", positions)
+	fmt.Printf("obstacleOptions: %v\n", obstacleOptions)
 
+}
+
+func canLoop(area [][]byte, guard *Guard, path map[string]bool) bool {
+	if area[guard.y][guard.x] != '.' {
+		return false
+	}
+
+	area[guard.y][guard.x] = '#'
+
+	res := false
+	testGuard := &Guard{
+		x:  guard.x - guard.dx,
+		y:  guard.y - guard.dy,
+		dx: guard.dx,
+		dy: guard.dy,
+	}
+	ah := len(area)
+	aw := len(area[0])
+
+	testPath := make(map[string]bool)
+
+	for {
+		if !testGuard.moveInArea(area, aw, ah) {
+			res = false
+			break
+		}
+
+		key := testGuard.getPathKey()
+
+		if path[key] || testPath[key] {
+			res = true
+			break
+		}
+		testPath[key] = true
+	}
+
+	area[guard.y][guard.x] = '.'
+	return res
 }
 
 func findGuard(area [][]byte) *Guard {
 	for y, row := range area {
 		for x, tile := range row {
-			dx, dy := 0, 0
-			switch tile {
-			case '^':
-				dy = -1
-			case 'v':
-				dy = 1
-			case '>':
-				dx = 1
-			case '<':
-				dx = -1
-
-			default:
-				continue
-			}
 			if tile == '^' {
 				return &Guard{
 					x:  x,
 					y:  y,
-					dx: dx,
-					dy: dy,
+					dx: 0,
+					dy: -1,
 				}
 			}
 		}
